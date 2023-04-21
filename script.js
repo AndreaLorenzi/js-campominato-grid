1cde5a44442985d13b1f3f02f4abdec2c0f19c03
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-04-21 16:19:51 +0200

    add campofiorito

---
+++ script.js
@@ -0,0 +1,31 @@
+// selezionare gli elementi del DOM necessari
+const eleGrid = document.querySelector('.grid');
+const eleBtnPlay = document.querySelector('#btn-play');
+
+// creare la funzione del bottone 
+eleBtnPlay.addEventListener('click',
+	function() {
+        // generare la griglia
+        createGrid(100, eleGrid);
+
+        // applicare gli event listeners a tutte le celle della griglia
+        const listCells = document.querySelectorAll('.cell');
+        for (let i = 0; i < listCells.length; i++) {
+            const cell = listCells[i];
+            cell.addEventListener('click',
+                function colorCell() {
+                    console.log(this);
+                    this.classList.toggle('clicked');
+                }
+            );
+        }
+
+        // funzione per creare la griglia
+
+        function createGrid(numCells, eleContainer) {
+            for (let i = 0; i < numCells; i++) {
+                eleContainer.innerHTML += `<div class="cell">${i+1}</div>`;
+            }
+        }
+    }
+)
