85c70f2984e0edae895b9673c1a245e8aa2cbe75
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-04-26 10:16:24 +0200

    adjustament

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-// selezionare gli elementi del DOM necessari
-const eleGrid = document.querySelector('.grid');
-const eleBtnPlay = document.querySelector('#btn-play');
-const eleDifficult = document.getElementById("difficult");
+   
+const eleButton = document.querySelector("#btn-play");
 
 
-
-
-      eleBtnPlay.addEventListener('click',
-	function() {
-
-        // generare la griglia a seconda della selezione del livello di difficoltà
-        
+eleButton.addEventListener("click", function () {
+    const eleGrid = document.querySelector('.grid');
+    const eleDifficulty = document.getElementById("level");
+    let value = eleDifficulty.options[eleDifficulty.selectedIndex].value;
+    console.log(value);
+    
+    // generare la griglia in base alla difficoltà scelta
+    if (value == "100") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add("grid_easy");
         createGrid(100, eleGrid);
-        // applicare gli event listeners a tutte le celle della griglia
-        const listCells = document.querySelectorAll('.cell');
-        for (let i = 0; i < listCells.length; i++) {
-            const cell = listCells[i];
-            cell.addEventListener('click',
-                function colorCell() {
-                    console.log(this);
-                    this.classList.toggle('clicked');
-                }
-            );
+    } else if (value == "81") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add( "grid_medium");
+        createGrid(81, eleGrid);
+    } else if (value == "49") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add("grid_hard");
+        createGrid(49, eleGrid);
+    }
+
+    const listCells = document.querySelectorAll('.cell');
+    // applicare gli event listeners a tutte le celle della griglia
+    for (let i = 0; i < listCells.length; i++) {
+        const cell = listCells[i];
+        cell.addEventListener('click', function() {
+            this.classList.toggle('clicked');
         }
+        )
+    }
 
-        // funzione per creare la griglia
-
-        function createGrid(numCells, eleContainer) {
-            for (let i = 0; i < numCells; i++) {
-                eleContainer.innerHTML += `<div class="cell">${i+1}</div>`;
-            }
+    function createGrid(numCells, eleContainer) {
+        for (let i = 0; i < numCells; i++) {
+        eleContainer.innerHTML += `<div class="cell">${i+1}</div>`;
         }
     }
-)
+    
+});
